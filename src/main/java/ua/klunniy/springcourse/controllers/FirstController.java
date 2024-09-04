f203abfcb457bf51ca59e37dd66d60e03e2981b1
serhii.klunnyi <serhii.klunnyi@appliedmethods.biz>
2024-09-04 12:57:09 +0300

    add edits

--- src/main/java/ua/klunniy/springcourse/controllers/FirstController.java
+++ src/main/java/ua/klunniy/springcourse/controllers/FirstController.java
@@ -1,5 +1,6 @@
 package ua.klunniy.springcourse.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @Controller
-@RequestMapping("/first")
+@RequestMapping("first")
 public class FirstController {
 
-    @GetMapping("/hello")
+    @GetMapping("hello")
     public String helloPage(HttpServletRequest request, Model model) {
+        log.info("*** " + Thread.currentThread().getName());
+
         final String name = request.getParameter("name");
         final String surname = request.getParameter("surname");
         final String message = "name: " + name + ", surname: " + surname;
@@ -24,7 +28,7 @@ public class FirstController {
         return "first/hello_world";
     }
 
-    @GetMapping("/goodbye")
+    @GetMapping("goodbye")
     public String goodByePage(@RequestParam(value = "name", defaultValue = "test-1") String name,
                               @RequestParam(value = "surname", required = false) String surname,
                               Model model) {
